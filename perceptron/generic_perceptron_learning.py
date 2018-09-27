622812641de340927e9002145727f02b250c343e
somak <somaktukai@gmail.com>
2018-09-27 18:16:21 +0530

    final update before moving on to numpy version

--- perceptron/generic_perceptron_learning.py
+++ perceptron/generic_perceptron_learning.py
@@ -33,16 +33,6 @@ class Perceptron(object):
         self._y_training_set = [0 if i.lower()!='iris-setosa' else 1 
                                     for i in data.iloc[0:, 4].values]
 
-        print(self._x_training_set)
-        print(self._y_training_set)
-
-        
-        self.draw_initial_plot(
-                                [item[0] for item in self._x_training_set],\
-                                [item[1] for item in self._x_training_set], \
-                                'Switch One', \
-                                'Switch Two'
-                            )                                        
         return len(self._x_training_set)
 
     def initialize_weights(self, number_of_weights):
@@ -63,12 +53,14 @@ class Perceptron(object):
         self.setup() 
         epoch_data = {}
         error = 0
+
         for epoch in range(self.epochs):
+
             for i in range(self.number_of_training_set):
                 _x = self._x_training_set[i]
                 _desired = self._y_training_set[i]
                 _weight = self.weights
-                guess = _weight[0]
+                guess = _weight[0] ## setting up the bias unit
 
                 for j in range(len(_x)):
                     guess += _weight[j+1] * _x[j]
@@ -77,23 +69,24 @@ class Perceptron(object):
 
                 ## i am going to reset all the weights
                 if error!= 0 :
+
+                    ## resetting the bias unit
                     self.weights[0] = error * self.learning_rate
 
                     for j in range(len(_x)):
                         self.weights[j+1] = self.weights[j+1] + error * self.learning_rate * _x[j]
 
-                    
+            #saving error at the end of the training set        
             epoch_data[epoch] = error
-        print(epoch_data)
-        self.draw_initial_plot(list(epoch_data.keys()), list(epoch_data.values()),'Epochs', 'Error')
+        
+        # print(epoch_data)
 
-class PlotMyData(object):
-    pass
+        self.draw_initial_plot(list(epoch_data.keys()), list(epoch_data.values()),'Epochs', 'Error')
 
 def runMyCode():
     learning_rate = 0.01
-    epochs = 10
-    random_generator_start = 0
+    epochs = 6
+    random_generator_start = -1
     random_generator_end = 1
     perceptron = Perceptron(epochs, learning_rate, [random_generator_start, random_generator_end])
     perceptron.learn()
